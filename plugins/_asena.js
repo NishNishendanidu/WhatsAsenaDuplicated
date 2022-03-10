4acf6089437345a4a0dc8fb2fdf98d373d4e91ce
Shen bro <96036613+NishNishendanidu@users.noreply.github.com>
2022-03-10 14:38:56 +0530

    Update _asena.js

--- plugins/_asena.js
+++ plugins/_asena.js
@@ -63,7 +63,7 @@ if (Config.WORKTYPE == 'private') {
                 }
             );
             await message.client.sendMessage(
-                message.jid,'●▬▬▬ *WhatsAsena Private* ▬▬▬●\n\n' + CMD_HELP, MessageType.text
+                message.jid,'●▬▬▬ *redblacky Private* ▬▬▬●\n\n' + CMD_HELP, MessageType.text
             );    
         } else {
             var CMD_HELP = '';
@@ -224,7 +224,7 @@ else if (Config.WORKTYPE == 'public') {
             );
             if (CMD_HELP === '') CMD_HELP += Lang.NOT_FOUND;
             await message.client.sendMessage(
-                message.jid,'●▬▬▬ *WhatsAsena Public* ▬▬▬●\n\n' + CMD_HELP, MessageType.text
+                message.jid,'●▬▬▬ *redblacky Public* ▬▬▬●\n\n' + CMD_HELP, MessageType.text
             );
         }
     }));
